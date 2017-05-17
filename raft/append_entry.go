7a40e5abd50182c774ecc64dbfca8d7e769cb896
zjsyhjh <zjsyhjh@gmail.com>
2017-05-17 20:37:20 +0800

    update append_entry.go

--- raft/append_entry.go
+++ raft/append_entry.go
@@ -4,7 +4,7 @@ package raft
 // add Log struct
 //
 type Log struct {
-	index int
+	Index int
 	Term  int
 	Cmd   interface{}
 }
@@ -13,9 +13,12 @@ type Log struct {
 // add AppendEntriesArgs RPC struct
 //
 type AppendEntriesArgs struct {
-	Term     int
-	LeaderID int
-	Entries  []Log
+	Term         int   // leader's term
+	LeaderID     int   // so follow can redirect clients
+	PrevLogIndex int   // index of log entry immediately preceding new ones
+	PrevLogTerm  int   // term of prevLogIndex entry
+	LeaderCommit int   //leader's commitIndex
+	Entries      []Log //log entries for store(empty for heartbeat)
 }
 
 //
@@ -51,7 +54,56 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 		DPrintf("server-%v reset term and convert to follower\n", rf.me)
 		rf.resetTermAndToFollower(args.Term)
 	}
+	//
 	go func() {
 		rf.heartbeatCh <- true
 	}()
+
+	rf.mu.Lock()
+	if args.LeaderCommit > rf.commitIndex {
+		rf.commitIndex = args.LeaderCommit
+		go rf.checkCommitIndexAndApplied()
+	}
+	rf.mu.Unlock()
+
+	//if entries is empty, heartbeat
+	if len(args.Entries) == 0 {
+		reply.Success = true
+		return
+	}
+
+	// replica log
+	if args.PrevLogIndex > len(rf.log) || rf.log[args.PrevLogIndex].Term != args.PrevLogTerm {
+		reply.Success = false
+	} else {
+		firstEntry := args.Entries[0]
+		if len(rf.log) > firstEntry.Index {
+			rf.log = rf.log[:(firstEntry.Index - 1)]
+		}
+		rf.log = append(rf.log, args.Entries...)
+		reply.Success = true
+	}
+	return
+}
+
+//
+func (rf *Raft) checkCommitIndexAndApplied() {
+	if rf.commitIndex > rf.lastApplied {
+		commitIndex := rf.commitIndex
+		lastApplied := rf.lastApplied
+
+		rf.mu.Lock()
+		rf.lastApplied = rf.commitIndex
+		rf.mu.Unlock()
+
+		for index := lastApplied + 1; index <= commitIndex; index++ {
+			msg := ApplyMsg{
+				Index:   rf.log[index].Index,
+				Command: rf.log[index].Cmd,
+			}
+			go func() {
+				rf.applyCh <- msg
+			}()
+		}
+	}
 }
